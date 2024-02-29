484116308345af6a190a296af5544edc0a0aafa5
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2024-02-29 12:42:54 +0100

    more gifs

--- core/src/com/gdx/game/course/introduction/GifDisplaySample.java
+++ core/src/com/gdx/game/course/introduction/GifDisplaySample.java
@@ -15,25 +15,35 @@ import com.gdx.game.course.introduction.common.SampleInfo;
 import com.gdx.game.course.introduction.utils.GdxUtils;
 import com.gdx.game.course.introduction.utils.GifDecoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GifDisplaySample extends SampleBase {
     public static final SampleInfo SAMPLE_INFO = new SampleInfo(GifDisplaySample.class);
-    private static final float WORLD_WIDTH = 10.4f; //world units
-    private static final float WORLD_HEIGHT = 12f; //world units
+    private static final float WORLD_WIDTH = 12.0f; //world units
+    private static final float WORLD_HEIGHT = 6.75f; //world units
     private OrthographicCamera camera;
     private Viewport viewport;
     private SpriteBatch spriteBatch;
-    Animation<TextureRegion> animation;
+    List<Animation<TextureRegion>> animations;
     float elapsed;
+    private int currentAnimationIndex;
 
 
     @Override
     public void create() {
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        animations = new ArrayList<>();
 
         camera = new OrthographicCamera();
         viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         spriteBatch = new SpriteBatch();
-        animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/pindeath.gif").read());
+        animations.add(GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/1.gif").read()));
+        animations.add(GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/2.gif").read()));
+        animations.add(GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/3.gif").read()));
+        animations.add(GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/4.gif").read()));
+        animations.add(GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("introduction/raw/5.gif").read()));
+
 
         Gdx.input.setInputProcessor(this);
     }
@@ -61,6 +71,17 @@ public class GifDisplaySample extends SampleBase {
     }
 
     private void draw() {
-        spriteBatch.draw(animation.getKeyFrame(elapsed), 0f, 0f, WORLD_WIDTH, WORLD_HEIGHT);
+        spriteBatch.draw(animations.get(currentAnimationIndex).getKeyFrame(elapsed), 0f, 0f, WORLD_WIDTH, WORLD_HEIGHT);
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        selectNextAnimation();
+        return true;
+    }
+
+    private void selectNextAnimation() {
+        elapsed = 0;
+        currentAnimationIndex = (currentAnimationIndex + 1) % animations.size();
     }
 }
