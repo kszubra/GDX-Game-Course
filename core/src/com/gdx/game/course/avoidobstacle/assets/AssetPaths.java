75f945df3d9cad9956bf57ef7f09c7408136d43c
KacperSz <kcpszubra@gmail.com>
2024-03-18 13:53:14 +0100

    texture atlas in game

--- core/src/com/gdx/game/course/avoidobstacle/assets/AssetPaths.java
+++ core/src/com/gdx/game/course/avoidobstacle/assets/AssetPaths.java
@@ -3,7 +3,7 @@ package com.gdx.game.course.avoidobstacle.assets;
 public class AssetPaths {
 
     public static final String UI_FONT = "avoidobstacle/ui/fonts/ui_font_32.fnt";
-    public static final String GAME_PLAY = "avoidobstacle/gameplay/gameplay.atlas";
+    public static final String GAME_PLAY = "core/obstacleavoid/assets/gameplay/gameplay.atlas";
     public static final String ANIMATION = "avoidobstacle/gameplay/blaidd.gif";
 
     private AssetPaths() {}
